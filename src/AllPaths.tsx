6f2a5e56b2fdbd0f27a1097ec2c239f41a2e1d97
Alexey Yurchenko <alexes.dev@gmail.com>
2022-09-15 14:44:20 +0300

    feat: add an AllPaths widget (#5)
    * feat: add an AllPaths widget
    
    * feat(AllPaths): show data for each path
    
    * fix(AllPaths): eslint warnings

---
+++ src/AllPaths.tsx
@@ -0,0 +1,66 @@
+import { useState, useReducer } from 'react';
+import { useQuery } from 'react-query';
+import { DataViewer } from './DataViewer';
+import * as s from './AllPaths.module.css';
+import { abciQuery, useNode } from './utils';
+
+type KeyResponse = {
+  children: string[];
+};
+
+const loadAllPaths = async (node: string, rootPath: string) => {
+  const { children: keys }: KeyResponse = await abciQuery(node, `/custom/vstorage/children/${rootPath}`);
+  const allPaths: string[] = [];
+
+  await Promise.all(
+    keys.map(async (key) => {
+      const subPath = rootPath === '' ? key : `${rootPath}.${key}`;
+      allPaths.push(subPath);
+
+      const subPaths = await loadAllPaths(node, subPath);
+      allPaths.push(...subPaths);
+    }),
+  );
+
+  return allPaths;
+};
+
+function OnePath({ value }: { value: string }) {
+  const [showData, toggleData] = useReducer((ss) => !ss, false);
+
+  return (
+    <div className={s.path}>
+      {value}{' '}
+      <button type="button" title="Toggle data" onClick={toggleData}>
+        d
+      </button>
+      {showData && <DataViewer path={value} />}
+    </div>
+  );
+}
+
+export function AllPaths() {
+  const node = useNode();
+  const [enabled, setEnabled] = useState(false);
+  const { isLoading, error, data } = useQuery(`allPaths:${node}`, () => loadAllPaths(node, ''), { enabled });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.toString()}</div>;
+  }
+
+  return (
+    <div className={s.root}>
+      <h3 className={s.title}>All paths</h3>
+      {!enabled && (
+        <button type="button" onClick={() => setEnabled(true)} disabled={isLoading}>
+          Load
+        </button>
+      )}
+      {data && data.map((path) => <OnePath key={path} value={path} />)}
+    </div>
+  );
+}
