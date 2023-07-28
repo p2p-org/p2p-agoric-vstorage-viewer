e4f48e296a23db46d39683559ad46ce8c223641b
Alexey Yurchenko <alexes.dev@gmail.com>
2023-07-28 19:32:35 +0300

    refactor(utils.ts): rm unused useEffect

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useQuery } from 'react-query';
 import { useStore } from './store';
 
